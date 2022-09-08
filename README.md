da8ee979ebfbf88cfbf597007da1353665b2dbcb
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2022-09-07 20:33:12 -0500

    added basic build instructions

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # rfs
 
 Passion project trying to rewrite [Seaweedfs](https://github.com/seaweedfs/seaweedfs) in pure rust. The main goal is simplicity, maintainability, and data safety.
+
+
+
+## Building from Source
+
+Install dependencies and build with cargo
+
+```bash
+sudo apt install libssl-dev cmake gcc fuse3 libfuse-dev pkg-config
+cd rfs
+cargo build --release
+```
+
+Build with docker
+
+```bash
+cd rfs
+bash ./builders/build-docker.sh
+```
+
+
+
--- builders/build-docker.sh
+++ builders/build-docker.sh
@@ -2,8 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../target
-docker build --no-cache $SCRIPT_DIR -t weed-build
-# docker build $SCRIPT_DIR -t weed-build
-docker run --rm --entrypoint cat weed-build  /go/bin/weed > $SCRIPT_DIR/../target/weed 
+# docker build --no-cache -t rfs-builder -f ./builders/Dockerfile $(dirname $SCRIPT_DIR) 
+docker build -t rfs-builder -f ./builders/Dockerfile $(dirname $SCRIPT_DIR) 
+docker run --rm --entrypoint cat rfs-builder  /bin/weed > $SCRIPT_DIR/../target/weed 
+docker run --rm --entrypoint cat rfs-builder  /bin/rfs > $SCRIPT_DIR/../target/rfs 
 chmod +x $SCRIPT_DIR/../target/weed
+chmod +x $SCRIPT_DIR/../target/rfs
+
+
 tar -C $SCRIPT_DIR/../target -czvf $SCRIPT_DIR/../target/weed.tar.gz weed 
